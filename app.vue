28283993dbe8ade1f7d03ec57995b478f23d9efe
Lithos-hub <lithos.contact@gmail.com>
2024-12-12 20:39:25 +0100

    install tailwind module

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="h-screen flex flex-col justify-center items-center">
+    <h1 class="text-blue-500 text-2xl">Hello world</h1>
   </div>
 </template>
